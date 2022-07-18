0f58bf6b886e50de5d200106b586e7c8c6facd0a
Rafael-Floriano <rafael.florianoc9@gmail.com>
2022-07-18 18:00:23 -0300

    cabeçalho php

---
+++ cabecalho/cabecalhoHome.php
@@ -0,0 +1,21 @@
+<header class="cabecalho">
+    <div class="cabecalho-logo">
+        <img src="./imagens/site/Logo_PNG_normal.png" class="logo-menu">
+        <div class="cabecalho-menu">
+            <ion-icon class="icones-cabecalho_menu" name="menu"></ion-icon>
+            <h2 class="botao-menu">MENU</h2>
+        </div>
+    </div>
+    <div class="cabecalho-pesquisa">
+        <form>
+            <div class="formulario-pesquisa">
+                <button class="botao-enviar" type="submit">
+                    <ion-icon name="search"></ion-icon>
+                </button>
+                <input class="botao-pesquisa" type="TEXT">
+            </div>
+        </form>
+        <ion-icon name="cart-outline" class="icones-cabecalho icone-cabecalho_pesquisa"></ion-icon>
+        <ion-icon name="person-circle-outline" class="icones-cabecalho icone-cabecalho_pesquisa"></ion-icon>
+    </div>
+</header>
--- home.php
+++ home.php
@@ -12,30 +12,17 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
-    <header class="cabecalho">
-        <div class="cabecalho-logo">
-            <img src="./imagens/site/Logo_PNG_normal.png" class="logo-menu">
-            <div class="cabecalho-menu">
-                <ion-icon class="icones-cabecalho_menu" name="menu"></ion-icon>
-                <h2 class="botao-menu">MENU</h2>
-            </div>
-        </div>
-        <div class="cabecalho-pesquisa">
-            <form>
-                <div class="formulario-pesquisa">
-                    <button class="botao-enviar" type="submit">
-                        <ion-icon name="search"></ion-icon>
-                    </button>
-                    <input class="botao-pesquisa" type="TEXT">
-                </div>
-            </form>
-            <ion-icon name="cart-outline" class="icones-cabecalho icone-cabecalho_pesquisa"></ion-icon>
-            <ion-icon name="person-circle-outline" class="icones-cabecalho icone-cabecalho_pesquisa"></ion-icon>
-        </div>
-    </header>
+    <!-- Cabeçalho da home -->
+    <?php
+    include 'cabecalho/cabecalhoHome.php'
+    ?>
+
+    <!-- Main da home -->
     <main>
 
     </main>
+
+    <!-- Rodapé da home -->
     <footer class="rodape">
         <div>
             <h3>Sugestão de Produto</h3>
