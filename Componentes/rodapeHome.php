6e8626e5314be875b359d6e0bdbe3add5c3f72a9
Rafael-Floriano <rafael.florianoc9@gmail.com>
2022-07-25 18:31:26 -0300

    rodape feito

--- Componentes/rodapeHome.php
+++ Componentes/rodapeHome.php
@@ -1,24 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <title>Rodape</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;700&display=swap');
-        *{
+
+        * {
             margin: 0px;
             padding: 0px;
             color: #323232;
             font-family: 'Inter', sans-serif;
+            font-size: 24px;
+            text-decoration: none;
         }
-        .rodape{
+
+        .rodape {
+            margin: 200px 0px 0px 0px;
+            padding: 30px 0px 0px 0px;
+            border-top: 1px solid #A3B3CC;
             display: flex;
             align-items: center;
             justify-content: center;
             gap: 180px;
         }
 
-        .coluna-um{
+        .coluna-um {
             display: flex;
             flex-direction: column;
             align-items: center;
@@ -26,34 +34,101 @@
             gap: 5px;
         }
 
-        .coluna-um-link a{
-            text-decoration: none;
-            color: #FFFFFF;
+        .logo{
+            max-width: 98px;
+        }
 
+        .coluna-um-link a {
+            color: #FFFFFF;
 
         }
 
-        .coluna-um-link{
+        .coluna-um-link {
             padding: 5px 15px;
             background-color: #FF83EF;
             border-radius: 85px;
+            transition: 0.20s;
 
         }
 
-        .icone{
+        .coluna-um-link:hover {
+            background-color: #EC56D9;
+        }
+
+        .icone {
             height: 35px;
         }
 
+        .coluna-dois {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 20px;
+        }
+
+        .texto-coluna-dois{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 5px;
+        }
+
+        .coluna-tres {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 20px;
+        }
+
+        .conteudo-coluna-tres{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 5px;
+        }
+
+        .noticias-coluna-tres{
+            margin: 50px 0px 0px 0px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 4px;
+
+        }
+
+        .noticias-coluna-tres input{
+            width:418px; 
+            height:38px;
+            background-color: #FFEFFD;
+            border: 1px solid #FF83EF;
+            border-radius: 105px;
+            padding: 0px 0px 0px 15px;
+            font-family: 'Inter', sans-serif;
+            font-size: 18px;
+            color: #323232;
+            outline: none;
+
+        }
+
+        /* .noticias-coluna-tres input:hover{
+            background-color: #FFEFFD;
+            border: 1px solid #FF83EF;
+            border-radius: 105px;
+            font-family: 'Inter', sans-serif;
+            color: #323232;
+        } */
+
     </style>
 </head>
+
 <body>
-    
+
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
     <footer class="rodape">
         <div class="coluna-um">
-            <img src="../imagens/site/Logo_PNG_normal.png" alt="logo-cherry-blossom" width="98px">
+            <img class="logo" src="./imagens/site/Logo_PNG_normal.png" alt="logo-cherry-blossom" width="98px">
             <h3 class="coluna-um-link"><a href="#">Sugestão de Produto</a></h3>
             <h3 class="coluna-um-link"><a href="#">Fale com o Vendedor</a></h3>
             <h4>Acompanhe nossas redes sociais</h4>
@@ -65,23 +140,34 @@
             </div>
         </div>
         <div class="coluna-dois">
-            <h3>Ajuda e Suporte</h3>
-            <p>Como comprar</p>
-            <p>Cuidados Com os Produtos</p>
-            <p>Visualização 3D</p>
-            <p>O que é Papercraft?</p>
-            <p>O que é Hama Beads?</p>
-            <p>O que é Macramê?</p>
-            <p>O que é miçanga?</p>
+            <div class="titulo-coluna-dois">
+                <h3>Ajuda e Suporte</h3>
+            </div>
+            <div class="texto-coluna-dois">
+                <p><a href="#">Como comprar</a></p>
+                <p><a href="#">Cuidados Com os Produtos</a></p>
+                <p><a href="#">Visualização 3D</a></p>
+                <p><a href="#">O que é Papercraft?</a></p>
+                <p><a href="#">O que é Hama Beads?</a></p>
+                <p><a href="#">O que é Macramê?</a></p>
+                <p><a href="#">O que é miçanga?</a></p>
+            </div>
         </div>
         <div class="coluna-tres">
-            <h3>Institucional</h3>
-            <p>Sobre Nós</p>
-            <p>Política de Privacidade</p>
-            <p>Termos de Uso e Navegação</p>
-            <h3>Se inscreva para receber novidades!</h3>
-            <input type="TEXT" name="email-noticias">
+            <div class="titulo-coluna-tres">
+                <h3>Institucional</h3>
+            </div>
+            <div class="conteudo-coluna-tres">
+                <p><a href="#">Sobre Nós</a></p>
+                <p><a href="#">Política de Privacidade</a></p>
+                <p><a href="#">Termos de Uso e Navegação</a></p> 
+            </div>
+            <div class="noticias-coluna-tres">
+                <h4>SE INSCREVA PARA RECEBER NOVIDADES!</h4>
+                <input type="email" name="email-noticias" placeholder="Digite seu email">
+            </div>
         </div>
     </footer>
 </body>
+
 </html>
--- home.php
+++ home.php
@@ -362,6 +362,10 @@
                     </div>
                 </div>
             </div>
+
+            <?php
+                include './Componentes/rodapeHome.php';
+            ?>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 
