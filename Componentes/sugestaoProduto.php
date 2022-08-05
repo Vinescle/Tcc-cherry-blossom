a2f599515f0e48f6a6ed55217c9b4b866d6d1da7
Rafael-Floriano <rafael.florianoc9@gmail.com>
2022-08-05 00:36:53 -0300

    Atualização da tela de sugestão

--- Componentes/sugestaoProduto.php
+++ Componentes/sugestaoProduto.php
@@ -3,11 +3,13 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content= "width=device-width, user-scalable=no">
 
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;700&display=swap');
 
         * {
+            box-sizing: border-box;
             color: #323232;
             font-family: 'Inter', sans-serif;
             margin: 0px;
@@ -16,6 +18,7 @@
 
         .popup {
             background-color: rgba(0, 0, 0, .5);
+            box-shadow: 4px 4px 10px -1px rgba(0, 0, 0, 0.32);
             position: fixed;
             top: 0;
             left: 0;
@@ -33,6 +36,14 @@
 
         }
 
+
+        .popup-icone-close{
+            font-size: 40px;
+            position: absolute;
+            top: 2vh;
+            left: 38vw;
+        }
+
         .popup-config {
             display: flex;
             flex-direction: column;
@@ -48,6 +59,7 @@
         }
 
         .popup-icone-svg{
+            margin: 4vh 0 0 0;
             font-size: 90px;
 
         }
@@ -80,22 +92,23 @@
         }
 
         .config-tamanho {
-            width: 642px;
-            height: 52px;
+            width: 34.5vw;
+            height: 5vh;
             border: solid 1px #FF97F2;
             border-radius: 20px;
-            padding-left: 0.8%;
-            font-size: 20px;
+            padding-left: .8vw;
+            font-size: 18px;
             font-family: 'Inter', sans-serif;
+            font-weight: 500;
             outline: none;
         }
 
         .popup-form-button {
-            font-size: 24px;
+            font-size: 22px;
             font-weight: bold;
             color: #FFFFFF;
-            width: 200px;
-            height: 44px;
+            width: 10.5vw;
+            height: 4.5vh;
             background-color: #FF97F2;
             border: none;
             border-radius: 20px;
@@ -118,7 +131,7 @@
 
     <div class="popup">
         <div class="popup-conteudo">
-            <div class="popup-fechar">X</div>
+            <div class="popup-fechar"><ion-icon class="popup-icone-close" name="close-outline"></ion-icon></div>
             <div class="popup-config">
                 <div class="popup-cabecalho">
                     <div class="popup-icone">
@@ -128,7 +141,7 @@
                     <p class="popup-texto">Compartilhe suas ideias para temas e produtos!</p>
                 </div>
                 <form class="popup-form">
-                    <input class="config-tamanho" type="text" name="sugestao" placeholder="  Digite sua sugestão">
+                    <input class="config-tamanho" type="text" name="sugestao" placeholder="Digite sua sugestão">
                     <input class="popup-form-button" type="submit">
                 </form>
             </div>
