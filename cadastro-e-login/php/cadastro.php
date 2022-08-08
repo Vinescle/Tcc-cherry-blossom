b0e0bb024de05d819b360c6924a427b1166bdb9e
Rafael-Floriano <rafael.florianoc9@gmail.com>
2022-08-08 20:32:00 -0300

    Deixando o código mais bonitinho

--- cadastro-e-login/php/cadastro.php
+++ cadastro-e-login/php/cadastro.php
@@ -22,223 +22,12 @@ if ($_SESSION['nome']) {
     $cpf = "";
     $receberEmails = "";
 }
-
-
-
 ?>
-
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/reset.css">
     <title>Cherry Blossom - Cadastro</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;700&display=swap');
-
-        * {
-            color: #323232;
-            font-family: 'Inter', sans-serif;
-            margin: 0px;
-            padding: 0px;
-        }
-
-        body {
-            background-image: url(../imagens/background/papel1.jpg);
-            background-repeat: repeat-y;
-            background-size: cover;
-            background-size: 100%;
-        }
-
-        main {
-            margin: 5% 0 8% 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .conteiner {
-            padding: 0% 3% 0.5% 3%;
-            width: 837px;
-            background: #FCFFD7;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 15px;
-            /* margin: 5% 25% 10% 25%; */
-            border-radius: 20px;
-            box-shadow: 4px 4px 10px -1px rgba(0, 0, 0, 0.32)
-        }
-
-        .conteiner-titulo {
-            width: 256px;
-            border-bottom: 2px solid #323232;
-        }
-
-        .conteiner-titulo p {
-            font-size: 24px;
-            font-weight: bold;
-            text-align: center;
-            margin: 20px 0 10px 0;
-        }
-
-        .config-tamanho {
-            width: 642px;
-            height: 52px;
-            border: solid 1px #FF97F2;
-            border-radius: 20px;
-            padding-left: 70px;
-            font-size: 20px;
-            font-family: 'Inter', sans-serif;
-            outline: none;
-        }
-
-        .nome::first-letter {
-            text-transform: uppercase;
-        }
-
-        .input-text {
-            font-size: 24px;
-            font-weight: 700;
-            margin-left: 14px;
-            font-family: 'Inter', sans-serif;
-        }
-
-        .input-corpo {
-            position: relative;
-            align-items: center;
-        }
-
-        form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 20px;
-        }
-
-        .input {
-            display: flex;
-            flex-direction: column;
-            gap: 5px;
-        }
-
-        .icon-config {
-            color: #FFFFFF;
-            width: 40px;
-            height: 40px;
-        }
-
-        .icon-eyes {
-            font-size: 30px;
-            cursor: pointer;
-        }
-
-
-        .input-button {
-            width: 62px;
-            height: 54px;
-            border-radius: 18px 0px 0px 18px;
-            border: none;
-            background-color: #FF97F2;
-            position: absolute;
-            top: 1px;
-            left: 1px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .input-eyes {
-            border: none;
-            background: transparent;
-            position: absolute;
-            top: 12px;
-            left: 670px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        .div-checkbox {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 2px;
-        }
-
-        .checkbox {
-            display: flex;
-            align-items: center;
-            gap: 2px;
-            font-family: 'Inter', sans-serif;
-            font-weight: 400;
-            font-size: 20px;
-        }
-
-        .checkbox input[type="checkbox"] {
-            cursor: pointer;
-            width: 30px;
-            height: 30px;
-            appearance: none;
-            -webkit-appearance: none;
-            border: 1px solid #FF97F2;
-            border-radius: 10px;
-            background-color: #FFFFFF;
-
-        }
-
-        .checkbox input[type="checkbox"]:checked:after {
-            display: block;
-            font-size: 20px;
-            font-family: 'Inter', sans-serif;
-            font-weight: 700;
-            content: "x";
-            display: flex;
-            align-items: center;
-            justify-content: center;
-
-        }
-
-
-        .checkbox a {
-            color: #1467FF;
-            font-weight: 400;
-        }
-
-        .form-end {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 10px;
-        }
-
-        .form-end-submit input {
-            font-size: 24px;
-            font-weight: bold;
-            color: #FFFFFF;
-            width: 200px;
-            height: 44px;
-            background-color: #FF97F2;
-            border: none;
-            border-radius: 20px;
-            cursor: pointer;
-            transition: 0.40S;
-
-        }
-
-        .form-end-submit input:hover {
-            background-color: #EC55D9;
-            cursor: pointer;
-        }
-
-        .form-end-text p {
-            font-size: 20px;
-            font-weight: 400;
-        }
-
-        .form-end-text a {
-            color: #1467FF;
-            font-weight: 400;
-        }
-    </style>
+    <link rel="stylesheet" href="../style/cadastro.css">
 </head>
 <body onload="checar()">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
@@ -341,10 +130,10 @@ if ($_SESSION['nome']) {
                 </div>
             </form>
         </div>
-        <script src="./senhaIcon.js"></script>
-        <script src="./mascaraData.js"></script>
-        <script src="./mascaraSenha.js"></script>
-        <script src="./checagemSenha.js"></script>
+        <script src="../js/senhaIcon.js"></script>
+        <script src="../js/mascaraData.js"></script>
+        <script src="../js/mascaraSenha.js"></script>
+        <script src="../js/checagemSenha.js"></script>
     </main>
 </body>
 
