3d37ac4596aa8c1f1248273faaa6a6efa2795099
Vini <Vinicius.bmartinho40@gmail.com>
2022-08-23 21:22:00 -0300

    Menu de administração em andamento

---
+++ Componentes/menuAdm.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="../css/reset.css" rel="stylesheet">
+    <link href="../css/menuAdm.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+    <div class="menu">
+            <div class="icones-principais">
+                <ion-icon name="home-outline" class="icones-menu"></ion-icon>
+
+                <ion-icon name="color-palette-outline" class="icones-menu"></ion-icon>
+
+                <ion-icon name="pricetag-outline" class="icones-menu"></ion-icon>
+
+                <ion-icon name="balloon-outline" class="icones-menu"></ion-icon>
+            </div>
+
+            <ion-icon name="settings-outline" class="icone-engrenagem"></ion-icon>
+    </div>
+</body>
+
+</html>
---
+++ homeAdm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Cherry Blossom - Adm</title>
+    <link href="./css/reset.css" rel="stylesheet">
+    <link href="./css/estilo-home.css" rel="stylesheet">
+    <link href="./css/menuAdm.css" rel="stylesheet">
+</head>
+
+<body>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+    <?php
+    include './Componentes/cabecalhoHomeAdm.php';
+    include './Componentes/menuAdm.php';
+    ?>
+
+</body>
+
+</html>
