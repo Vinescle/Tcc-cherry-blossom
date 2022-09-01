71b849f835e6ef4df7d057c2b20f40d644abaeb8
Rafael-Floriano <rafael.florianoc9@gmail.com>
2022-09-01 18:01:01 -0300

    Hover dos botões do adm menu

--- Componentes/menuAdm.php
+++ Componentes/menuAdm.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .icones-menu{
+            transition: 0.4s;
+        }
+        .icones-menu:hover{
+            background-color: #EC55D9;
+            color:white;
+        }
+
+        .icone-engrenagem{
+            transition: 0.4s;
+        }
+
+        .icone-engrenagem:hover{
+            background-color: #EC55D9;
+            color:white;
+        }
+    </style>
+</head>
+<body>
 <div class="menu">
     <div class="icones-principais">
         <ion-icon name="home-outline" class="icones-menu"></ion-icon>
@@ -11,3 +38,7 @@
 
     <ion-icon name="settings-outline" class="icone-engrenagem"></ion-icon>
 </div>
+</body>
+</html>
+
+
