64b2ad6fb9f40ce37bdc48250c6a61b687c839e4
Vini <Vinicius.bmartinho40@gmail.com>
2022-10-29 22:00:14 -0300

    Rodapé sobre concluído

--- rodape-sobre/termos.php
+++ rodape-sobre/termos.php
@@ -1,27 +1,32 @@
+<?php
+include '../conexao.php';
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
-    <title>Termos</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link href="<?php echo $rota; ?>/assets/imagens/logo.png" rel="shortcut icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="<?php echo $rota; ?>/assets/css/base.css" rel="stylesheet">
+    <link href="<?php echo $rota; ?>/assets/css/home.css" rel="stylesheet">
+    <link href="<?php echo $rota; ?>/assets/css/componentes/menu-cabecalho.css" rel="stylesheet">
+    <link href="<?php echo $rota; ?>/assets/css/componentes/rodape.css" rel="stylesheet">
 
-    <link href="../css/reset.css" rel="stylesheet">
-    <link href="../css/rodape-itens/termos.css" rel="stylesheet">
-    <link href="../css/estilo-home.css" rel="stylesheet">
+    <link href="<?php echo $rota; ?>/assets/css/rodape-sobre/termos.css" rel="stylesheet">
+
+    <title>Cherry Blossom - Home</title>
 </head>
 
 <body>
-
     <?php
-    session_start();
-
-    if (isset($_SESSION['logado'])) {
-        include '../Componentes/cabecalhoHomeLogado.php';
-    } else if (!isset($_SESSION['logado'])) {
-        include '../Componentes/cabecalhoHome.php';
-    }
+    include('../componentes/menu-cabecalho.php');
     ?>
-
+    
     <main>
         <div class="conteudo-principal">
             <div class="div-titulo">
@@ -89,12 +94,12 @@
     </main>
 
     <?php
-    include '../Componentes/rodapeHome.php';
-    include '../Componentes/sugestaoProduto.php';
+    include('../componentes/rodape.php');
     ?>
 
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <?php
+    include('../imports.php');
+    ?>
 </body>
 
 </html>
