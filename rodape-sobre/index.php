f604519b874dcc612a547c3df6681940a72efbc6
Rafael-Floriano <rafael.florianoc9@gmail.com>
2022-10-29 21:51:41 -0300

    Oqueé.php

--- rodape-sobre/index.php
+++ rodape-sobre/index.php
@@ -1,34 +1,31 @@
 <?php
 include '../conexao.php';
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
-    <title>Produtos</title>
-
-    <link href="<?php echo $rota;?>/assets/css/reset.css" rel="stylesheet">
-    <link href="<?php echo $rota;?>/assets/css/rodape-sobre/oQueE.css" rel="stylesheet">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link href="<?php echo $rota; ?>/assets/imagens/logo.png" rel="shortcut icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link href="<?php echo $rota; ?>/assets/css/base.css" rel="stylesheet">
     <link href="<?php echo $rota; ?>/assets/css/home.css" rel="stylesheet">
     <link href="<?php echo $rota; ?>/assets/css/componentes/menu-cabecalho.css" rel="stylesheet">
     <link href="<?php echo $rota; ?>/assets/css/componentes/rodape.css" rel="stylesheet">
+
+    <link href="<?php echo $rota; ?>/assets/css/rodape-sobre/oQueE.css" rel="stylesheet">
+
+    <title>Cherry Blossom - Home</title>
 </head>
 
 <body>
-
     <?php
-    // session_start();
-
-    if (isset($_SESSION['logado'])) {
-        include "../componentes/menu-cabecalho.php";
-    } else if (!isset($_SESSION['logado'])) {
-        include "../componentes/menu-cabecalho.php";
-    }
+    include('../componentes/menu-cabecalho.php');
     ?>
-
     <main>
         <div class="conteudo-principal">
             <div class="div-papercraft">
@@ -36,7 +33,7 @@ include '../conexao.php';
                     <h4 class="subtitulo">O QUE É PAPERCRAFT?</h4>
                 </div>
 
-                <img class="imagens" src="<?php echo $rota;?>/assets/imagens/site/papercraft.png">
+                <img class="imagens" src="../assets/imagens/site/papercraft.png">
 
                 <p class="texto">Papercraft é um método de construir objetos tridimensionais utilizando papel, uma arte parecida com o origami, mas se diferencia por utilizar dobraduras, cortes, colagem, moldagem e até mesmo criação de camadas. Apesar de parecerem frágeis por serem de papel, as peças são feitas com papéis mais grossos e possuem um acabamento em verniz e goma laca, os tornando mais resistentes.</p>
 
@@ -48,7 +45,7 @@ include '../conexao.php';
                     <h4 class="subtitulo">O QUE É HAMA BEADS?</h4>
                 </div>
 
-                <img class="imagens" src="<?php echo $rota;?>/assets/imagens/site/hama-beads.png">
+                <img class="imagens" src="../assets/imagens/site/hama-beads.png">
 
                 <p class="texto">Hama beads são objetos feitos utilizando pequenas peças plásticas unidas, podendo-se criar qualquer coisa em um estilo pixel art. Com eles, podem ser criados diversos objetos, como acessórios, decorações, porta-copos, chaveiros e quadros.</p>
             </div>
@@ -58,7 +55,7 @@ include '../conexao.php';
                     <h4 class="subtitulo">O QUE É MACRAMÊ?</h4>
                 </div>
 
-                <img class="imagens" src="<?php echo $rota;?>/assets/imagens/site/macrame.png">
+                <img class="imagens" src="../assets/imagens/site/macrame.png">
 
                 <p class="texto">O macramê é uma técnica de tecelagem manual que utiliza nós para criar diferentes tipos de peças. Vários tipos de fios são usados, dando origem a diferentes itens, desde peças de roupas até itens decorativos.</p>
             </div>
@@ -68,20 +65,19 @@ include '../conexao.php';
                     <h4 class="subtitulo">O QUE É MIÇANGA?</h4>
                 </div>
 
-                <img class="imagens" src="<?php echo $rota;?>/assets/imagens/site/micanga.png">
+                <img class="imagens" src="../assets/imagens/site//micanga.png">
 
                 <p class="texto">Miçangas são pequenos objetos decorativos de diferentes tamanhos, texturas, acabamentos, cores e materiais, podendo ser de pedra, osso, cerâmica, madeira, borracha, concha, plástico ou vidro. Geralmente são utilizadas na produção de acessórios.</p>
             </div>
         </div>
     </main>
-
     <?php
-    include "../componentes/rodape.php";
-    // include '../.php';
+    include('../componentes/rodape.php');
     ?>
 
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <?php
+    include('../imports.php');
+    ?>
 </body>
 
 </html>
